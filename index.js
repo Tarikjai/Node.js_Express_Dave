d791c5fae033511ce7f0c35b9a6d545a86e13158
Tarik J <tarik.jaidani@gmail.com>
2024-09-17 01:18:49 +0200

    write a file opération

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ fs.readFile(path.join(__dirname, 'files','starter.txt'), 'utf8', (err,data)=>{
     console.log(data.toString())
 })
 
+console.log("Hello Tarik")
+
+
+fs.writeFile(path.join(__dirname, 'files','replay.txt'), 'Writing my file',(err)=>{
+    if (err) throw err;
+    console.log("write complete")
+})
+
+
 process.on('uncaughtException', err => {
     console.error(`There was an uncaught error: ${err}`);
     process.exit(1)
