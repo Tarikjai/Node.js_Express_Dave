ba69215d86d623f782ab6dddf823d2b262e05b23
Tarik J <tarik.jaidani@gmail.com>
2024-09-16 17:56:08 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const fs =require('fs');
+
+
+fs.readFile('./files/starter.txt', 'utf8', (err,data)=>{
+    if (err) throw err;
+    console.log(data.toString())
+})
+
+fs.readFile('./files/starter.txt', 'utf8', (err,data)=>{
+    if (err) throw err;
+    console.log(data.toString())
+})
