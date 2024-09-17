55486951fc6773edb4405443b9ccb55a677bf4cd
Tarik J <tarik.jaidani@gmail.com>
2024-09-17 13:45:44 +0200

    Replace call back with async await

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
  
-const fsPromies = require('fs').promises;
+const fsPromises = require('fs').promises;
 const path = require('path')
 
 
 const fileOps = async ()=> {
     try {
-        const data = await fsPromies.readFile(path.join(__dirname, 'files','starter.txt'), "utf8")
+        const data = await fsPromises.readFile(path.join(__dirname, 'files','starter.txt'), "utf8")
         console.log(data);
+        await fsPromises.writeFile(path.join(__dirname, 'files','promiseWrite.txt'), data)
+        await fsPromises.appendFile(path.join(__dirname, 'files','promiseWrite.txt'), '\n\nNice to meet you.')
+        await fsPromises.rename(path.join(__dirname, 'files','promiseWrite.txt'), path.join(__dirname, 'files','promiseComplete.txt'))
+        const newData = await fsPromises.readFile(path.join(__dirname, 'files','promiseComplete.txt'), "utf8")
+        console.log(newData);
     } catch (error) {
         console.error(err);
     }
